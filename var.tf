36f9e6d03c6964e2847748dce7e884c041b4bae0
Tofan-Jena <47977011+Tofan-Jena@users.noreply.github.com>
2020-03-22 14:55:09 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,25 @@
+# Backend###
+variable "client_id_b" {}
+variable "secret_id_b" {}
+variable "tenant_id_b" {}
+variable "subscription_id_b" {}
+
+# FrontEnd
+variable client_id_f {}
+variable secret_id_f {}
+variable tenant_id_f {}
+variable subscription_id_f {}
+
+
+
+variable "counts" {
+    default = 2
+}
+
+variable "RGName-Prefix" {
+  default = "RG"
+}
+
+
+
+
